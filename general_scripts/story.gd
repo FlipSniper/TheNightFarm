168ce639ca003b9c5c85debb8931f2bba7327bfe
FlipSniper <m-navaneeth21@st-bernards.slough.sch.uk>
2025-10-25 11:25:01 +0100

    story + more sounds

--- general_scripts/story.gd
+++ general_scripts/story.gd
@@ -2,13 +2,14 @@ extends Node
 var label = 0
 var prev_label = 0
 var skippable = false
-
+@onready var particle = $Node3D/CPUParticles3D
 func _ready() -> void:
 	$Story/fade.visible = true
 	$Story/AnimationPlayer.play("fade")
 	await get_tree().create_timer(2).timeout
 	$Story/fade.visible = false
 	skippable = true
+	$Sounds/calm_wind.play()
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("click") and !$Story/AnimationPlayer.is_playing() and skippable:
@@ -21,11 +22,13 @@ func _process(delta: float) -> void:
 			label2()
 		if label == 3:
 			label3()
+		if label == 4:
+			label4()
 
 func label1():
 	skippable = false
 	$Story/Label.visible = true
-	$Narrator1.play()
+	$Sounds/Narrator1.play()
 	await get_tree().create_timer(5).timeout
 	skippable = true
 
@@ -33,13 +36,24 @@ func label2():
 	$Story/Label.visible = false
 	skippable = false
 	$Story/Label2.visible = true
-	$Eli1.play()
-	await get_tree().create_timer(8).timeout
+	$Sounds/Eli1.play()
+	await get_tree().create_timer(6.3).timeout
 	skippable = true
+	$Sounds/rage_wind.play()
+	particle.amount = 500
 func label3():
 	$Story/Label2.visible = false
 	skippable = false
 	$Story/Label3.visible = true
-	$Eli2.play()
+	$Sounds/Eli2.play()
 	await get_tree().create_timer(5).timeout
+	skippable = true
+func label4():
+	$Sounds/rage_wind.volume_db = -13.0
+	$Story/Label3.visible = false
+	skippable = false
+	$Story/Label4.visible = true
+	$Sounds/Narrator2.play()
+	await get_tree().create_timer(5).timeout
+	$Sounds/grr.play()
 	skippable = true
