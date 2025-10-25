2ca49545de3b8523071b6d9a06321ffb844e0aee
FlipSniper <m-navaneeth21@st-bernards.slough.sch.uk>
2025-10-25 10:03:31 +0100

    Fixed 1280X720 bug

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,5 +1,15 @@
 extends Node
 
+func _ready() -> void:
+	$Main_Menu/fade.visible = false
 
 func play() -> void:
+	print("ye mint")
+	$Main_Menu/fade.visible = true
+	$Main_Menu/AnimationPlayer.play("fade")
+	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://scenes/story.tscn")
+
+
+func _on_button_mouse_entered() -> void:
+	print("ye mint")
